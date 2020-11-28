afaf576fe60dad09d1cd2c528e5f8ab9bb7ec475
Zap-Master <kim.lidiya2011@gmail.com>
2020-11-28 19:51:28 +0000

    Implemented first exercise - reverseAllWords

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -58,6 +58,10 @@ function reverseWord(word) {
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Add your code here!
+  for (let i = 0; i < words.length; i++){
+    words[i] = reverseWord(words[i]);
+  }
+  return words;
 }
 
 function countLinuxUsers(users) {
