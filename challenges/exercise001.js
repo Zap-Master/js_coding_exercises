32988970e4295f6e8d807806075bc0474fb4e4a8
Zap-Master <kim.lidiya2011@gmail.com>
2020-11-28 20:39:16 +0000

    Implemented first exercise - countLinuxUsers

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -67,6 +67,14 @@ function reverseAllWords(words) {
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   // Add your code here!
+  var result = 0;
+  for (var i = 0; i < users.length; i++){
+    if (users[i].type === "Linux"){
+      result = result + 1;
+      console.log(result);
+    }
+  }
+  return result;
 }
 
 function getMeanScore(scores) {
