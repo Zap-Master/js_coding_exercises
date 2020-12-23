01fd34e31ced1e698ea392c61fbdfad91f6c80d7
Zap-Master <kim.lidiya2011@gmail.com>
2020-12-23 17:16:47 +0000

    tidy up Exercise 002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,25 +1,21 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
   return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
   return (person.city === "Manchester");
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
   return (Math.ceil(people/40));
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
-  var result = 0;
+  let result = 0;
   for (let i = 0; i < arr.length; i ++){
     if (arr[i] === "sheep") {
       result ++;
@@ -30,8 +26,6 @@ function countSheep(arr) {
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  //var result = "";
   return ((person.address.city === "Manchester" && person.address.postCode.substring(0,1) === "M"));
   
 }
