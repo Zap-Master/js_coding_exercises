78e348e60c33aac465c81164439e8182598f2a6e
Zap-Master <kim.lidiya2011@gmail.com>
2020-12-23 18:12:44 +0000

    tidy up Exercise 003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,20 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
-  var result = [];
-  var i = 0;
-  nums.forEach(element => {
-    result[i] = Math.pow(nums[i],2);
-    i ++;
+  const squaredNums = nums.map(function (num) {
+    return num * num;
   });
-  return result;
+  return squaredNums;
 }
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
-  var result = "";
-  //result = words.reduce((a,b) => a + b.substring(0,1).toUpperCase() + b.substring(1,b.length), "");
-  for (let i = 0; i < words.length; i ++){
-    if (i === 0){
+  let result = "";
+  for (let i = 0; i < words.length; i++) {
+    if (i === 0) {
       result = words[i];
     }
-    else{
-      result = result + words[i].substring(0,1).toUpperCase() + words[i].substring(1,words[i].length);
+    else {
+      result = result + words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length);
     }
   }
   return result;
@@ -28,41 +22,40 @@ function camelCaseWords(words) {
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
-  var result = 0;
-  var i = 0;
+  let result = 0;
   people.forEach(element => {
-    result = result + people[i].subjects.length;
-    i ++;
+    result = result + element.subjects.length;
   });
   return result;
-  
+
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
-  var result = false;
-  for (let i = 0; i < menu.length; i++) {
-    result = menu[i].ingredients.includes(ingredient);
-    if (result) {
-      break;
+  let result = false;
+  menu.forEach(meal =>{
+    //console.log(meal.ingredients  + "     " + ingredient + "    " + meal.ingredients.includes(ingredient));
+    if (meal.ingredients.includes(ingredient)) {
+      result = true;
+      return;
     }
-  }
+  });
+  
   return (result);
+  
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
-  var result = [];
-  for (let i = 0; i < arr1.length; i ++){
-    if (arr2.includes(arr1[i]) && !result.includes(arr1[i])) {
-      result.push(arr1[i]);
+  let result = [];
+  arr1.forEach(element =>{
+    if (arr2.includes(element) && !result.includes(element)) {
+      result.push(element);
     }
-  }
+
+  });
   return result.sort();
 }
 
